2fabb7bc24ee0f60669ec93bc9c26ee94f39f041
Henrique Selau de Oliveira <hso.softwares@gmail.com>
2026-07-26 06:22:43 -0300

    Initial commit: backend Spring Boot

---
+++ nearby-marketplace-backend/src/main/java/com/ipollo/nearby_marketplace/NearbyMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.ipollo.nearby_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyMarketplaceApplication.class, args);
+	}
+
+}
---
+++ nearby-marketplace-backend/src/test/java/com/ipollo/nearby_marketplace/NearbyMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipollo.nearby_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbyMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
