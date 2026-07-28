e600b1e1ca055561988e4a743c932d54eb15b389
Henrique Selau de Oliveira <hso.softwares@gmail.com>
2026-07-28 06:25:52 -0300

    fix: typo NotBlank tag to NoNull in RegisterRequest DTO

--- nearby-marketplace-backend/src/main/java/com/ipollo/nearby_marketplace/dto/RegisterRequest.java
+++ nearby-marketplace-backend/src/main/java/com/ipollo/nearby_marketplace/dto/RegisterRequest.java
@@ -2,10 +2,11 @@ package com.ipollo.nearby_marketplace.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record RegisterRequest(
         @NotBlank String name,
         @NotBlank @Email String email,
         @NotBlank String password,
-        @NotBlank Long cityId
+        @NotNull Long cityId
 ) {}
